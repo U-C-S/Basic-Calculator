f2d5bcb0ba8a6661995dca2088f605ee8ad098e1
U-C-S <ucs3721@outlook.com>
2020-08-24 17:11:32 +0530

    Added meta data to the build

--- Basic Calculator/Properties/AssemblyInfo.cs
+++ Basic Calculator/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Basic Calculator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple Calculator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("UCS")]
 [assembly: AssemblyProduct("Basic Calculator")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
