f797413088cd50199b1c1cfe389c1c33c64db894
U-C-S <ucs3721@outlook.com>
2020-08-26 15:33:09 +0530

    v2.1

--- Basic Calculator/MainWindow.xaml.cs
+++ Basic Calculator/MainWindow.xaml.cs
@@ -27,5 +27,39 @@ namespace Basic_Calculator
         private void Draggable(object sender, RoutedEventArgs e) => this.DragMove();
         private void Close(object sender, RoutedEventArgs e) => this.Close();
 
+        private void Btn_num(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (boxResult.Text.Length == 1 && boxResult.Text.IndexOf("0") == 0)
+            {
+                boxResult.Text = btn.Content.ToString();
+            }
+            else
+            {
+                boxResult.Text += btn.Content.ToString();
+            }
+        }
+        private void Operator(object sender, RoutedEventArgs e)
+        {
+            Button oper = (Button)sender;
+            boxMain.Text += $" {boxResult.Text} {oper.Content}";
+        }
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            boxMain.Text = "";
+            boxResult.Text = "0";
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            if(boxResult.Text.Length > 1)
+            {
+                boxResult.Text = boxResult.Text.Substring(0, boxResult.Text.Length - 1);
+            }
+            else{
+                boxResult.Text = "0";
+            }
+        }
+
     }
 }
