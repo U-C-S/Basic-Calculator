6c6230a0a7005671f5aa10042f175e738a4c429c
U-C-S <ucs3721@outlook.com>
2020-08-27 16:48:48 +0530

    v3 - A bugged working Calculator

--- Basic Calculator/MainWindow.xaml.cs
+++ Basic Calculator/MainWindow.xaml.cs
@@ -24,6 +24,11 @@ namespace Basic_Calculator
         {
             InitializeComponent();
         }
+        public bool operAtTheEnd = false;
+        private float num1 = 0, num2 = 0, result;
+        private int numOp = 0;
+        private string CrOp;
+
         private void Draggable(object sender, RoutedEventArgs e) => this.DragMove();
         private void Close(object sender, RoutedEventArgs e) => this.Close();
 
@@ -38,25 +43,138 @@ namespace Basic_Calculator
             {
                 boxResult.Text += btn.Content.ToString();
             }
+
+            //test
+            /*
+            if(num1 == 0)
+            {
+                num1 = int.Parse(btn.Content.ToString());
+            }
+            else if (boxResult.Text.Contains("."))
+            {
+                num1 = num1 + 0.1F;
+            }
+            else
+            {
+                num1 = (num1 * 10) + int.Parse(btn.Content.ToString());
+            }
+            */
         }
+        private void Btn_dot(object sender, RoutedEventArgs e)
+        {
+            if (!boxResult.Text.Contains("."))
+            {
+                boxResult.Text += ".";
+            }
+            
+        }
+
         private void Operator(object sender, RoutedEventArgs e)
         {
+            numOp += 1;
             Button oper = (Button)sender;
-            boxMain.Text += $" {boxResult.Text} {oper.Content}";
+            OperAtEnd(oper.Content.ToString());
+            if (numOp >= 2 && num2 == 0)
+            {
+                num2 = float.Parse(boxResult.Text);
+            }
+            if (num1 == 0)
+            {
+                num1 = float.Parse(boxResult.Text);
+                CrOp = oper.Content.ToString();
+            }
+            
+            if(numOp >= 2)
+            {
+                num1 = cal(num1, CrOp, num2);
+                boxResult.Text = $"{num1}";
+            }
         }
+
+        private float cal(float x, string a, float y)
+        {
+            switch (a)
+            {
+                case "+":
+                    result = x + y;
+                    break;
+                case "-":
+                    result = x - y;
+                    break;
+                case "x":
+                    result = x * y;
+                    break;
+                case "/":
+                    result = x / y;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+        private void OperAtEnd(string x)
+        {
+            if(boxMain.Text == "" && boxResult.Text == "0")
+            {
+
+            }
+            else if ((boxMain.Text.EndsWith("+") || boxMain.Text.EndsWith("-") || boxMain.Text.EndsWith("x") || boxMain.Text.EndsWith("/")) && boxResult.Text == "0")
+            {
+                boxMain.Text = boxMain.Text.Substring(0, boxMain.Text.Length - 1) + x;
+            }
+            else
+            {
+                boxMain.Text += $" {boxResult.Text} {x}";
+               // boxResult.Text = "0";
+            }
+        }
+        private void Btn_equal(object sender, RoutedEventArgs e)
+        {
+            boxMain.Text += boxResult.Text;
+            /*
+            int si = boxMain.Text.IndexOf("+");
+            string a = boxMain.Text.Substring(0, si - 2);
+            string o = boxMain.Text.Substring(si,si+1);
+            string b = boxMain.Text.Substring(si + 3,si+4);
+
+            switch (o)
+            {
+                case "+ " :
+                    boxResult.Text = $"{float.Parse(a) + float.Parse(b)}";
+                    break;
+                case " -" :
+                    boxResult.Text = $"{float.Parse(a) - float.Parse(b)}";
+                    break;
+                case " x":
+                    boxResult.Text = $"{float.Parse(a) * float.Parse(b)}";
+                    break;
+                case " /":
+                    boxResult.Text = $"{float.Parse(a) / float.Parse(b)}";
+                    break;
+                default:
+                    boxResult.Text = "Error";
+                    break;
+            }
+            */
+        }
+
         private void Clear(object sender, RoutedEventArgs e)
         {
+            num1 = 0;
+            num2 = 0;
+            result = 0;
             boxMain.Text = "";
             boxResult.Text = "0";
         }
 
         private void Back(object sender, RoutedEventArgs e)
         {
-            if(boxResult.Text.Length > 1)
+            if (boxResult.Text.Length > 1)
             {
                 boxResult.Text = boxResult.Text.Substring(0, boxResult.Text.Length - 1);
             }
-            else{
+            else
+            {
                 boxResult.Text = "0";
             }
         }
