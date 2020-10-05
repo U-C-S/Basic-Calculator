c44b75197e74f1d0835f4b063d215a0b1d94670b
U-C-S <ucs3721@outlook.com>
2020-10-05 15:14:08 +0530

    v8.62

--- Basic Calculator/Properties/AssemblyInfo.cs
+++ Basic Calculator/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Simple Calculator")]
+[assembly: AssemblyTitle("Basic Calculator")]
 [assembly: AssemblyDescription("A basic Calculator with basic Operations")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("UCS")]
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.6.1.0")]
-[assembly: AssemblyFileVersion("8.6.1.0")]
+[assembly: AssemblyVersion("8.6.2.0")]
+[assembly: AssemblyFileVersion("8.6.2.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
