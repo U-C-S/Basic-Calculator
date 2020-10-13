87cfd92b81c68ffc78077aa175f566176004f38b
U-C-S <ucs3721@outlook.com>
2020-10-13 17:35:45 +0530

    v8.63.1

--- Basic Calculator/MainWindow.xaml.cs
+++ Basic Calculator/MainWindow.xaml.cs
@@ -16,126 +16,15 @@ namespace Basic_Calculator
         bool resultShowing = false;
 
         //UI Events
-        private void Draggable(object sender, RoutedEventArgs e) => DragMove();
-        private void Close(object sender, RoutedEventArgs e) => Close();
-        private void Btn_num(object sender, RoutedEventArgs e) => NumberFunc((sender as Button).Content.ToString());
-        private void Btn_dot(object sender, RoutedEventArgs e) => DotCheck();
-        private void Operator(object sender, RoutedEventArgs e) => CheckandCal((sender as Button).Content.ToString());
-        private void Btn_equal(object sender, RoutedEventArgs e) => TheEnterKey();
-        private void Clear(object sender, RoutedEventArgs e) => ClearEverything();
-        private void Back(object sender, RoutedEventArgs e) => BackSpace();
-
-
-        //UI Event Functions--------
-        private void NumberFunc(string i)
-        {
-            if (boxResult.Text.Length == 1 && boxResult.Text.IndexOf("0") == 0)
-                boxResult.Text = i;
-            else if (resultShowing)
-                boxResult.Text = i;
-            else
-                boxResult.Text += i;
-
-            resultShowing = false;
-        }
-
-        private void DotCheck()
-        {
-            if (!boxResult.Text.Contains(".")) { boxResult.Text += "."; }
-        }
-
-        //Calculation logic--
-        private void CheckandCal(string x)
-        {
-            if (resultShowing && boxMain.Text != "")
-            {
-                boxMain.Text = boxMain.Text.Substring(0, boxMain.Text.Length - 1) + x;
-                operStore[numOp - 1] = x;
-            }
-            else
-            {
-                boxMain.Text += $" {boxResult.Text} {x}";
-                operStore.Add(x);
-                numOp += 1;
-            }
-
-            if (numOp == 1)
-            {
-                TheResult = float.Parse(boxMain.Text.Substring(0, boxMain.Text.Length - 2));
-                resultShowing = true;
-            }
-
-            if (numOp >= 2 && !resultShowing)
-            {
-                TheResult = Calu(TheResult, operStore[numOp - 2], float.Parse(boxResult.Text));
-                boxResult.Text = $"{TheResult}";
-                resultShowing = true;
-            }
-        }
-
-        private float Calu(float x, string a, float y)
-        {
-            float resu;
-            switch (a)
-            {
-                case "+":
-                    resu = x + y; break;
-                case "-":
-                    resu = x - y; break;
-                case "x":
-                    resu = x * y; break;
-                case "/":
-                    resu = x / y; break;
-                default: throw new Exception("Operator Error");
-            }
-            return resu;
-        }
-
-        private void BackSpace()
-        {
-            if (resultShowing)
-                boxResult.Text = "0";
-            else if (boxResult.Text.Length > 1)
-                boxResult.Text = boxResult.Text.Substring(0, boxResult.Text.Length - 1);
-            else
-                boxResult.Text = "0";
-        }
-
-        private void TheEnterKey()
-        {
-            if (resultShowing)
-            {
-                boxResult.Text = $"{TheResult}";
-            }
-            else if (numOp > 0)
-            {
-                float num1 = float.Parse(boxResult.Text);
-                TheResult = Calu(TheResult, operStore[numOp - 1], num1);
-                boxResult.Text = $"{TheResult}";
-                resultShowing = true;
-            }
-            else
-            {
-                boxResult.Text = boxResult.Text;
-            }
-            numOp = 0;
-            TheResult = 0;
-            boxMain.Text = "";
-            operStore.Clear();
-        }
-
-        private void ClearEverything()
-        {
-            numOp = 0;
-            TheResult = 0;
-            boxMain.Text = "";
-            boxResult.Text = "0";
-            operStore.Clear();
-            resultShowing = false;
-        }
-
-        //For Keyboard Functionality-----
-        private void Keyboard_press(object sender, KeyEventArgs e)
+        void Draggable(object sender, RoutedEventArgs e) => DragMove();
+        void Close(object sender, RoutedEventArgs e) => Close();
+        void Btn_num(object sender, RoutedEventArgs e) => NumberFunc((sender as Button).Content.ToString());
+        void Btn_dot(object sender, RoutedEventArgs e) => DotCheck();
+        void Operator(object sender, RoutedEventArgs e) => CheckandCal((sender as Button).Content.ToString());
+        void Btn_equal(object sender, RoutedEventArgs e) => TheEnterKey();
+        void Clear(object sender, RoutedEventArgs e) => ClearEverything();
+        void Back(object sender, RoutedEventArgs e) => BackSpace();
+        void Keyboard_press(object sender, KeyEventArgs e)
         {
             switch (e.Key)
             {
@@ -180,6 +69,112 @@ namespace Basic_Calculator
                 case Key.Multiply: CheckandCal("x"); break;
                 case Key.Divide: CheckandCal("/"); break;
             }
+        }
+
+        //Calculation logic--
+        void NumberFunc(string i)
+        {
+            if (boxResult.Text.Length == 1 && boxResult.Text.IndexOf("0") == 0)
+                boxResult.Text = i;
+            else if (resultShowing)
+                boxResult.Text = i;
+            else
+                boxResult.Text += i;
+
+            resultShowing = false;
+        }
+
+        void DotCheck()
+        {
+            if (!boxResult.Text.Contains(".")) { boxResult.Text += "."; }
+        }
+
+        void CheckandCal(string x)
+        {
+            if (resultShowing && boxMain.Text != "")
+            {
+                boxMain.Text = boxMain.Text.Substring(0, boxMain.Text.Length - 1) + x;
+                operStore[numOp - 1] = x;
+            }
+            else
+            {
+                boxMain.Text += $" {boxResult.Text} {x}";
+                operStore.Add(x);
+                numOp += 1;
+            }
+
+            if (numOp == 1)
+            {
+                TheResult = float.Parse(boxMain.Text.Substring(0, boxMain.Text.Length - 2));
+                resultShowing = true;
+            }
+            else if (numOp > 1 && !resultShowing)
+            {
+                TheResult = Calu(TheResult, operStore[numOp - 2], float.Parse(boxResult.Text));
+                boxResult.Text = $"{TheResult}";
+                resultShowing = true;
+            }
+        }
+
+        float Calu(float x, string a, float y)
+        {
+            float resu;
+            switch (a)
+            {
+                case "+":
+                    resu = x + y; break;
+                case "-":
+                    resu = x - y; break;
+                case "x":
+                    resu = x * y; break;
+                case "/":
+                    resu = x / y; break;
+                default: throw new Exception("Operator Error");
+            }
+            return resu;
+        }
+
+        void BackSpace()
+        {
+            if (resultShowing)
+                boxResult.Text = "0";
+            else if (boxResult.Text.Length > 1)
+                boxResult.Text = boxResult.Text.Substring(0, boxResult.Text.Length - 1);
+            else
+                boxResult.Text = "0";
+        }
+
+        void TheEnterKey()
+        {
+            if (resultShowing)
+            {
+                boxResult.Text = $"{TheResult}";
+            }
+            else if (numOp > 0)
+            {
+                float num1 = float.Parse(boxResult.Text);
+                TheResult = Calu(TheResult, operStore[numOp - 1], num1);
+                boxResult.Text = $"{TheResult}";
+                resultShowing = true;
+            }
+            else
+            {
+                boxResult.Text = boxResult.Text;
+            }
+            numOp = 0;
+            TheResult = 0;
+            boxMain.Text = "";
+            operStore.Clear();
+        }
+
+        void ClearEverything()
+        {
+            numOp = 0;
+            TheResult = 0;
+            boxMain.Text = "";
+            boxResult.Text = "0";
+            operStore.Clear();
+            resultShowing = false;
         }
     }
 }
